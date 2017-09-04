bee2ba363b73552098b41bd53e0a92fab8a9dcca
monkypuzzle <paul.newsam@gmail.com>
2017-09-03 17:14:08 -0700

    Adds bootstrap 3, moves bootstrap css import to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

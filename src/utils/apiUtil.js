5357afd6f7658559d1d33fac1f80a0ec7ff78c93
monkypuzzle <paul.newsam@gmail.com>
2017-09-05 16:33:56 -0700

    Refines buildRequestUri utility

--- src/utils/apiUtil.js
+++ src/utils/apiUtil.js
@@ -1,16 +1,29 @@
-export default function apiUtil(){
-  const apiRoot = "https://cors-anywhere.herokuapp.com/https://canvas.instructure.com/api/v1/";
+function ApiUtil() {
+  const apiRoot = "https://cors-anywhere.herokuapp.com/https://sweetwaterschools.instructure.com/api/v1/";
+  const token = `access_token=${process.env.REACT_APP_ACCESS_TOKEN}`;
 
-  let buildUrl = function(args){
-    let url = apiRoot + "courses";
-    // if (args.courseId) {
-    //   url += `/${courseId}`;
-    // }
-    url += `?access_token=${process.env.REACT_APP_ACCESS_TOKEN}`;
-    return(url);
+  let l = "https://sweetwaterschools.instructure.com/api/v1/courses/1246894/users?enrollment_type[student]=student&access_token=12~shgVKqc411vZQPcvDf9yXTNCcJC9obj0D4P1u0QjRLgFHYB4CSjff4KgfLKzLjK7";
+
+
+  function buildRequestUri(args) {
+    let url = apiRoot + args.resource;
+    if (args.resourceId) { url = url + "/" + args.resourceId; }
+    if (args.nestedResource) { url = url + "/" + args.nestedResource; }
+    if (args.queries) {
+      for (let i = 0; i < args.queries.length; i++) {
+        url = url + "?" + args.queries[i];
+      }
+      url = url + "&" + token;
+    } else {
+      url = url + "?" + token;
+    }
+    console.log(url);
+    return (url);
   }
 
   return({
-    buildUrl: buildUrl
+    buildRequestUri: buildRequestUri
   });
 }
+
+export default ApiUtil;

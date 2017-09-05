eeaad439b136721cd8cd38ac7f9f010275c3a6b2
monkypuzzle <paul.newsam@gmail.com>
2017-09-04 18:40:15 -0700

    Adds CourseContainer component

---
+++ src/containers/CourseContainer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class CourseContainer extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+    fetch();
+  }
+
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default CourseContainer;

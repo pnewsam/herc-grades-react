3b8f55e649010b2877621669fa0d7d1f3af5b5ce
monkypuzzle <paul.newsam@gmail.com>
2017-09-04 18:37:57 -0700

    Abstracts login page layout into CenteredCard component

--- src/components/bulma/CenteredCard.jsx
+++ src/components/bulma/CenteredCard.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class CenteredCard extends Component {
-  render(){
-    return (
-      <div className="columns">
-        <div className="column is-10 is-offset-1">
-          <div className="card">
-            <div className="card-content">
-              {this.props.children}
-            </div>
-          </div> 
-        </div>
+const CenteredCard = (props) => {
+  return (
+    <div className="columns">
+      <div className="column is-10 is-offset-1">
+        <div className="card">
+          <div className="card-content">
+            {props.children}
+          </div>
+        </div> 
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default CenteredCard;

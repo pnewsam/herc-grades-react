838f476e05b935cf4c28bf0a2c2e117bf78b05df
monkypuzzle <paul.newsam@gmail.com>
2017-09-10 13:22:07 -0700

    Adds phases to form, ConfigurationPhase component

--- src/components/SeatingChart/SeatingChartForm.jsx
+++ src/components/SeatingChart/SeatingChartForm.jsx
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import SeatingChart from "./SeatingChart";
 import CardColumn from "../bulma/CardColumn";
 import StudentList from "../StudentList/StudentList";
-import SeatingChartConfigurationForm from "./SeatingChartConfigurationForm";
+import ConfigurationPhase from "./ConfigurationPhase";
+import SeatingPhase from "./SeatingPhase";
 
 class SeatingChartForm extends Component {
   constructor(props){
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReturn = this.handleReturn.bind(this);
     this.calculateSeatWidth = this.calculateSeatWidth.bind(this);
     this.state = {
       seats: {},
@@ -16,7 +18,8 @@ class SeatingChartForm extends Component {
       numRows: 0,
       numCols: 0,
       width: 0,
-      seatWidth: 0
+      seatWidth: 0,
+      phase: 1
     };
   }
 
@@ -34,6 +37,15 @@ class SeatingChartForm extends Component {
         width: cW
       });
     });
+  }
+
+  handleReturn(e){
+
+    let prevPhase = this.state.phase - 1;
+    this.setState({
+      phase: prevPhase
+    });
+    console.log(this.state);
   }
 
   handleChange(e){
@@ -54,29 +66,50 @@ class SeatingChartForm extends Component {
     let w = this.calculateSeatWidth(this.state.width);
     this.setState({
       seats: s,
-      seatWidth: w
+      seatWidth: w,
+      phase: 2
     });
   }
 
   render(){
-    return(
-      <div className="columns">
-        <CardColumn is={9}>
-          <div ref={container => { this.container = container }}>
-            <SeatingChart seats={this.state.seats} seatWidth={this.state.seatWidth}/>
+    switch (this.state.phase) {
+      case 1:
+        return(
+          <div className="columns">
+            <CardColumn is={8}>
+              <div ref={container => { this.container = container }}>
+                <SeatingChart seats={this.state.seats} seatWidth={this.state.seatWidth}/>
+              </div>
+            </CardColumn>
+            <CardColumn is={4}>
+            <ConfigurationPhase
+              handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}
+              numRows={this.state.numRows}
+              numCols={this.state.numCols}
+              numStudents={Object.keys(this.state.students).length}
+            />
+            </CardColumn>
           </div>
-        </CardColumn>
-        <CardColumn is={3}>
-          <SeatingChartConfigurationForm
-            handleSubmit={this.handleSubmit}
-            handleChange={this.handleChange}
-            numRows={this.state.numRows}
-            numCols={this.state.numCols}
-          />
-          <StudentList students={this.state.students}/>
-        </CardColumn>
-      </div>
-    )
+        )
+      case 2:
+        return(
+          <div className="columns">
+            <CardColumn is={8}>
+              <div ref={container => { this.container = container }}>
+                <SeatingChart seats={this.state.seats} seatWidth={this.state.seatWidth}/>
+              </div>
+            </CardColumn>
+            <CardColumn is={4}>
+              <SeatingPhase
+                handleReturn={this.handleReturn}
+                students={this.state.students}
+                seats={this.state.seats}
+              />
+            </CardColumn>
+          </div>
+        )
+    }
   }
 }
 
